7b3ec384ab4d26489907bc90150a16b7637df188
Anderson Silva <andersonsilva8609@gmail.com>
2022-09-23 03:28:40 -0300

    upload de projeto

---
+++ src/main/java/br/com/api/parkingcontroll/repositories/ParkingSpotRepository.java
@@ -0,0 +1,16 @@
+package br.com.api.parkingcontroll.repositories;
+
+import br.com.api.parkingcontroll.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
